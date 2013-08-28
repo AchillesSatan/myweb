8948aa0dbed2cafddd46a8fd214634bfc5368192
AchillesSatan <achillessatan@gmail.com>
2013-08-28 12:59:51 +0800

    css
    css

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -3,4 +3,9 @@ module BlogsHelper
     	user = User.find(user_id)
     	user.name
 	end
+
+	def get_month month
+		m = %w{ January February March April May June July August September October November December}
+		m[month-1]
+	end
 end
